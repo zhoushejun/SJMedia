e1a1df922b187573125ed75d48c264bfe1fd9a04
shejunzhou <965678322@qq.com>
2016-02-24 10:27:18 +0800

    Init

---
+++ SJMedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJMedia
+//
+//  Created by shejun.zhou on 16/2/24.
+//  Copyright © 2016年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJMedia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SJMedia
+//
+//  Created by shejun.zhou on 16/2/24.
+//  Copyright © 2016年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
